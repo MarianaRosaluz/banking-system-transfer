31f1924da69bc243b745fe63a260ce82b66e6464
mariana.rosaluz <mariana.rosaluz@ifood.com.br>
2022-04-24 21:52:47 -0300

    chore: create a new module to service

--- business/src/main/java/br/rosaluz/banking/system/transfer/service/TransferServiceImpl.java
+++ business/src/main/java/br/rosaluz/banking/system/transfer/service/TransferServiceImpl.java
@@ -1,11 +1,9 @@
 package br.rosaluz.banking.system.transfer.service;
 
-import br.rosaluz.banking.system.transfer.repository.TransferRepository;
 import br.rosaluz.banking.system.transfer.model.Transfer;
+import br.rosaluz.banking.system.transfer.repository.TransferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class TransferServiceImpl implements TransferService{
